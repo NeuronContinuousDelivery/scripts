b8dca24d54ed1c9257d0c7730e55fa118270aefd
god <164798848@qq.com>
2018-03-19 21:03:59 +0800

    code

--- _debug.sh
+++ _debug.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-REACT_APP_WEB_PATH=/web/accounts/login \
 PORT=3001 \
-PUBLIC_URL='123' \
 neuron-react-debug.sh
--- _release.sh
+++ _release.sh
@@ -3,5 +3,5 @@
 NAME="neuron-account-login-web" \
 VERSION="v1.0.0" \
 PORT=3001 \
-WEB_PATH=/web/accounts/login \
+PUBLIC_URL=/web/accounts/login \
 neuron-react-release.sh
