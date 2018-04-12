42946d13124fb3788756e21bdec4459da67f545f
god <164798848@qq.com>
2018-04-12 12:19:34 +0800

    rebuild login

---
+++ dev/nginx/debug.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cp ./nginx.conf /usr/local/etc/nginx/nginx.conf
+nginx -s reload
--- dev/nginx/release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-PROD_IP=${PROD_IP}
-
-scp ./nginx.conf root@${PROD_IP}:/etc/nginx/
-
-ssh root@${PROD_IP} << 1234567890abcdef
-nginx -s reload
-1234567890abcdef
